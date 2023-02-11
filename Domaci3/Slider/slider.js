c0e5cad80a26d866deacc79bb4abc87405d274c3
nemanjalaki <sone.lakicevic@gmail.com>
2023-02-11 13:04:55 +0100

    left/right keyboard option, close option

--- Domaci3/Slider/slider.js
+++ Domaci3/Slider/slider.js
@@ -1,22 +1,23 @@
 let next = document.querySelector(".next");
 let previous = document.querySelector(".previous");
+let closePicture = document.querySelector(".closebtn");
 let images = document.querySelectorAll('.image');
+let imageContainer = document.querySelector('.container');
 let mainImage = document.querySelector('.main');
 const totalImages = images.length;
 let newIndex = 0;
+let mouseInside = false;
+
 
 function displayImage(index){
-    console.log(index);
     images[index].classList.add('main');
 }
 
 function nextSlide(){
     images.forEach((element, index) => {
         if (element.classList.contains('main') && index < (totalImages - 1)){
-            console.log(index);
             images[index].classList.remove('main');
             newIndex = index + 1;
-            console.log(newIndex);
         }
     });
     displayImage(newIndex);
@@ -25,10 +26,8 @@ function nextSlide(){
 function previousSlide(){
     images.forEach((element, index) => {
         if (element.classList.contains('main') && index > 0){
-            console.log(index);
             images[index].classList.remove('main');
             newIndex = index - 1;
-            console.log(newIndex);
         }
     });
     displayImage(newIndex);
@@ -36,6 +35,29 @@ function previousSlide(){
 
 next.addEventListener("click", nextSlide);
 previous.addEventListener("click", previousSlide);
+closePicture.addEventListener("click", function(){
+    images.forEach((element) => {
+        element.classList.remove('main');
+    });
+}); 
 
 
+imageContainer.addEventListener('mouseover', function(e) {
+    mouseInside = true;
+});
+
+imageContainer.addEventListener('mouseout', function(e) {
+    mouseInside = false;
+});
+
+document.addEventListener('keydown', function(e){
+    if (mouseInside === true){
+        if(e.key == 'ArrowRight') {
+            nextSlide();
+        }
+        else if(e.key == 'ArrowLeft') {
+            previousSlide();
+        }
+    }
+});
 
